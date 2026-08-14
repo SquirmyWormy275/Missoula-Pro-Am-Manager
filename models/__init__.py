ce196016006f1e2a1117f15f9ca59d2dfff20b40
SquirmyWormy275 <alex.j.kaper@gmail.com>
2026-08-14 14:16:45 -0700

    feat(shadow): add operational persistence schema

--- models/__init__.py
+++ models/__init__.py
@@ -20,6 +20,15 @@ from .print_tracker import PrintTracker
 from .pro_event_rank import ProEventRank
 from .relay import RelayState, RelayTeam, RelayTeamEvent, RelayTeamMember
 from .school_captain import SchoolCaptain
+from .shadow_handicap import (
+    CompetitorExternalIdentity,
+    ShadowContextObservation,
+    ShadowHandicapRun,
+    ShadowLifecycleTransition,
+    ShadowOutcomeRevision,
+    ShadowReceiptRevision,
+    ShadowSettlementOutbox,
+)
 from .team import Team
 from .tournament import Tournament
 from .tournament_event import TournamentEvent
@@ -56,4 +65,11 @@ __all__ = [
     'BirlingMatch',
     'BirlingFall',
     'BirlingPlacement',
+    'CompetitorExternalIdentity',
+    'ShadowHandicapRun',
+    'ShadowLifecycleTransition',
+    'ShadowReceiptRevision',
+    'ShadowContextObservation',
+    'ShadowOutcomeRevision',
+    'ShadowSettlementOutbox',
 ]
